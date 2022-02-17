2af996a59b9e15da7e30b43d5013680f3fa14f7c
msgulsever <selimhan43@gmail.com>
2022-02-17 22:13:15 +0300

    Initial commit

---
+++ Countdown mobile app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Countdown mobile app
+//
+//  Created by Muhammed Selimhan gülsever on 14.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Countdown mobile app/ekleCV.swift
@@ -0,0 +1,42 @@
+//
+//  ekleCV.swift
+//  Countdown mobile app
+//
+//  Created by Muhammed Selimhan gülsever on 15.02.2022.
+//
+
+import UIKit
+
+class ekleCV: UIViewController {
+
+    @IBOutlet weak var bilgilbl: UILabel!
+    
+    @IBOutlet weak var resimekle: UIImageView!
+    
+    @IBOutlet weak var bilgigirtf: UITextField!
+    
+    @IBOutlet weak var tarih: UIDatePicker!
+    
+    @IBOutlet weak var bilgi3: UILabel!
+    @IBOutlet weak var bilgi2: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func mavibutton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func redbutton(_ sender: Any) {
+    }
+    
+
+    @IBAction func greenbutton(_ sender: Any) {
+    }
+    
+    @IBAction func yellowbutton(_ sender: Any) {
+    }
+}
