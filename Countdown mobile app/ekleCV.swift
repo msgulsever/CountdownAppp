49496683a96a082fb4d9dbd3ced4f06a053e4531
msgulsever <selimhan43@gmail.com>
2022-03-09 12:49:43 +0300

    deva
    devam

--- Countdown mobile app/ekleCV.swift
+++ Countdown mobile app/ekleCV.swift
@@ -13,17 +13,59 @@ class ekleCV: UIViewController {
     
     @IBOutlet weak var resimekle: UIImageView!
     
+    
+    @IBOutlet weak var tekrarsw: UISwitch!
+    
+    @IBOutlet weak var bildirimsw: UISwitch!
+    
     @IBOutlet weak var bilgigirtf: UITextField!
-    
-    @IBOutlet weak var tarih: UIDatePicker!
-    
+    let datePicker = UIDatePicker()
+    @IBOutlet weak var tarihtf: UITextField!
     @IBOutlet weak var bilgi3: UILabel!
     @IBOutlet weak var bilgi2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tekrarsw.setOn(false, animated: true)
+        bildirimsw.setOn(false, animated: true)
+        tekrarsw.tintColor = .red
+        tekrarsw.onTintColor = .green
+        tekrarsw.thumbTintColor = .darkGray
+        tekrarsw.backgroundColor = .brown
+        
+        createDataPicker()
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func tekrarbutton(_ sender: Any) {
+        if tekrarsw.isOn {
+            
+        }
+    }
+    
+    @IBAction func bildirimbutton(_ sender: Any) {
+    }
+    
+    
+    func createDataPicker(){
+        //toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        // bar button
+        let donebutton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: nil)
+        toolbar.setItems([donebutton], animated: true)
+        // assing toolbar
+        tarihtf.inputAccessoryView = toolbar
+        // assing date picker to the tf
+        tarihtf.inputView = datePicker
+    }
+    
+    @objc func donepressed(){
+        tarihtf.text = "\(datePicker.date)"
+        self.view.endEditing(true)
+    }
+    
+    
     
 
     @IBAction func mavibutton(_ sender: Any) {
@@ -39,4 +81,6 @@ class ekleCV: UIViewController {
     
     @IBAction func yellowbutton(_ sender: Any) {
     }
+    
+    
 }
